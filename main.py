09a9322bcbf127ce98ff2946118bf8edb54ae922
mofeomotosho8@gmail.com <mofeomotosho8@gmail.com>
2022-06-16 20:47:17 -0300

    Adding right password logic

--- main.py
+++ main.py
@@ -10,29 +10,29 @@ givenNotes = []
 contas = ["mofe", "matheus", "bruno", "jan"]
 password = input('Please enter your passsword: ')
 
-for i in contas:
-    if i == password:
-        num = (input('Please enter an ammount: '))
 
-        while amountGiven < int(num):
-            if currency50 > 0 and amountGiven+50 <= int(num):
-                givenNotes.append(50)
-                currency50 -= 1
-                amountGiven += 50
-            elif currency10 > 0 and amountGiven+10 <= int(num):
-                givenNotes.append(10)
-                currency10 -= 1
-                amountGiven += 10
-            elif currency5 > 0 and amountGiven+5 <= int(num):
-                givenNotes.append(5)
-                currency5 -= 1
-                amountGiven += 5
-            elif currency1 > 0 and amountGiven+1 <= int(num):
-                givenNotes.append(1)
-                currency1 -= 1
-                amountGiven += 1
-    else:
-        print('Wrong Password !!')
+if password in contas:
+    num = (input('Please enter an ammount: '))
+
+    while amountGiven < int(num):
+        if currency50 > 0 and amountGiven+50 <= int(num):
+            givenNotes.append(50)
+            currency50 -= 1
+            amountGiven += 50
+        elif currency10 > 0 and amountGiven+10 <= int(num):
+            givenNotes.append(10)
+            currency10 -= 1
+            amountGiven += 10
+        elif currency5 > 0 and amountGiven+5 <= int(num):
+            givenNotes.append(5)
+            currency5 -= 1
+            amountGiven += 5
+        elif currency1 > 0 and amountGiven+1 <= int(num):
+            givenNotes.append(1)
+            currency1 -= 1
+            amountGiven += 1
+else:
+    print('Wrong Password !!')
 
 
 print(givenNotes)
