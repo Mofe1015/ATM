388f7e3c667ed5e8a8d39bc7ceaf2c9984681aaa
mofeomotosho8@gmail.com <mofeomotosho8@gmail.com>
2022-06-15 22:38:18 -0300

    Adding temporay password logic

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
-from contextlib import nullcontext
-from locale import currency
 
-
-print('Hello Worlds')
 currency50 = 100
 currency10 = 100
 currency5 = 100
 currency1 = 100
 
-
-num = (input('Please enter an ammount: '))
 amountGiven = 0
 givenNotes = []
 
-while amountGiven < int(num):
-    if currency50 > 0 and amountGiven+50 <= int(num):
-        givenNotes.append(50)
-        currency50 -= 1
-        amountGiven += 50
-    elif currency10 > 0 and amountGiven+10 <= int(num):
-        givenNotes.append(10)
-        currency10 -= 1
-        amountGiven += 10
-    elif currency5 > 0 and amountGiven+5 <= int(num):
-        givenNotes.append(5)
-        currency5 -= 1
-        amountGiven += 5
-    elif currency1 > 0 and amountGiven+1 <= int(num):
-        givenNotes.append(1)
-        currency1 -= 1
-        amountGiven += 1
+contas = ["mofe", "matheus", "bruno", "jan"]
+password = input('Please enter your passsword: ')
+
+for i in contas:
+    if i == password:
+        num = (input('Please enter an ammount: '))
+
+        while amountGiven < int(num):
+            if currency50 > 0 and amountGiven+50 <= int(num):
+                givenNotes.append(50)
+                currency50 -= 1
+                amountGiven += 50
+            elif currency10 > 0 and amountGiven+10 <= int(num):
+                givenNotes.append(10)
+                currency10 -= 1
+                amountGiven += 10
+            elif currency5 > 0 and amountGiven+5 <= int(num):
+                givenNotes.append(5)
+                currency5 -= 1
+                amountGiven += 5
+            elif currency1 > 0 and amountGiven+1 <= int(num):
+                givenNotes.append(1)
+                currency1 -= 1
+                amountGiven += 1
+    else:
+        print('Wrong Password !!')
+
 
 print(givenNotes)
 print(sum(givenNotes))
