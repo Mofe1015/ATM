6e9c33c388771487ecf4cb68bccc9f4e99910808
mofeomotosho8@gmail.com <mofeomotosho8@gmail.com>
2022-06-15 22:17:11 -0300

    ...

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from locale import currency
 
 
 print('Hello Worlds')
-currency50 = 1000
-currency10 = 1000
-currency5 = 1000
-currency1 = 1000
+currency50 = 100
+currency10 = 100
+currency5 = 100
+currency1 = 100
 
 
 num = (input('Please enter an ammount: '))
@@ -16,19 +16,19 @@ givenNotes = []
 while amountGiven < int(num):
     if currency50 > 0 and amountGiven+50 <= int(num):
         givenNotes.append(50)
-        currency50 - 1
+        currency50 -= 1
         amountGiven += 50
     elif currency10 > 0 and amountGiven+10 <= int(num):
         givenNotes.append(10)
-        currency10 - 1
+        currency10 -= 1
         amountGiven += 10
     elif currency5 > 0 and amountGiven+5 <= int(num):
         givenNotes.append(5)
-        currency5 - 1
+        currency5 -= 1
         amountGiven += 5
     elif currency1 > 0 and amountGiven+1 <= int(num):
         givenNotes.append(1)
-        currency1 - 1
+        currency1 -= 1
         amountGiven += 1
 
 print(givenNotes)
