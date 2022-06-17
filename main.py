3bf5935b453352d7173d3544f1fe6b9da43e3324
mofeomotosho8@gmail.com <mofeomotosho8@gmail.com>
2022-06-16 21:42:10 -0300

    Only making withdrawal when password and username is righ

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ givenNotes = []
 contas = [["mofe", "matheus", "bruno", "jan"],
           ["1234", "4567", "8910", "8911"]]
 
+
 username = input('Please enter your username: ')
 
 if username in contas[0]:
@@ -18,29 +19,29 @@ if username in contas[0]:
     password = input('Please enter your password: ')
 
     if password == contas[1][userPosition]:
-        print('correct password')
+        print('WELCOME', username)
+
+        num = (input('Please enter an ammount: '))
+
+        while amountGiven < int(num):
+            if currency50 > 0 and amountGiven+50 <= int(num):
+                givenNotes.append(50)
+                currency50 -= 1
+                amountGiven += 50
+            elif currency10 > 0 and amountGiven+10 <= int(num):
+                givenNotes.append(10)
+                currency10 -= 1
+                amountGiven += 10
+            elif currency5 > 0 and amountGiven+5 <= int(num):
+                givenNotes.append(5)
+                currency5 -= 1
+                amountGiven += 5
+            elif currency1 > 0 and amountGiven+1 <= int(num):
+                givenNotes.append(1)
+                currency1 -= 1
+                amountGiven += 1
     else:
         print('wrong password')
-
-    num = (input('Please enter an ammount: '))
-
-    while amountGiven < int(num):
-        if currency50 > 0 and amountGiven+50 <= int(num):
-            givenNotes.append(50)
-            currency50 -= 1
-            amountGiven += 50
-        elif currency10 > 0 and amountGiven+10 <= int(num):
-            givenNotes.append(10)
-            currency10 -= 1
-            amountGiven += 10
-        elif currency5 > 0 and amountGiven+5 <= int(num):
-            givenNotes.append(5)
-            currency5 -= 1
-            amountGiven += 5
-        elif currency1 > 0 and amountGiven+1 <= int(num):
-            givenNotes.append(1)
-            currency1 -= 1
-            amountGiven += 1
 else:
     print('Wrong username !!')
 
