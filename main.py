ebb2f4d5114c20c4aaacf62bcffbfffbe916d01d
mofeomotosho8@gmail.com <mofeomotosho8@gmail.com>
2022-06-17 22:04:49 -0300

    Adding right looping logic

--- main.py
+++ main.py
@@ -6,44 +6,46 @@ currency1 = 100
 
 amountGiven = 0
 givenNotes = []
+num = 1
 
 contas = [["mofe", "matheus", "bruno", "jan"],
           ["1234", "4567", "8910", "1112"]]
 
 
-username = 'mofe'
+while int(num) != 0:
 
-while username in contas[0]:
     username = input('Please enter your username: ')
 
-    userPosition = contas[0].index(username)
-    password = input('Please enter your password: ')
+    if username in contas[0]:
 
-    if password == contas[1][userPosition]:
-        print('WELCOME', username)
+        userPosition = contas[0].index(username)
+        password = input('Please enter your password: ')
 
-        num = (input('Please enter an ammount: '))
+        if password == contas[1][userPosition]:
+            print('WELCOME', username)
 
-        while amountGiven < int(num):
-            if currency50 > 0 and amountGiven+50 <= int(num):
-                givenNotes.append(50)
-                currency50 -= 1
-                amountGiven += 50
-            elif currency10 > 0 and amountGiven+10 <= int(num):
-                givenNotes.append(10)
-                currency10 -= 1
-                amountGiven += 10
-            elif currency5 > 0 and amountGiven+5 <= int(num):
-                givenNotes.append(5)
-                currency5 -= 1
-                amountGiven += 5
-            elif currency1 > 0 and amountGiven+1 <= int(num):
-                givenNotes.append(1)
-                currency1 -= 1
-                amountGiven += 1
-        print(givenNotes)
-        print(sum(givenNotes))
+            num = (input('Please enter an ammount: '))
+
+            while amountGiven < int(num):
+                if currency50 > 0 and amountGiven+50 <= int(num):
+                    givenNotes.append(50)
+                    currency50 -= 1
+                    amountGiven += 50
+                elif currency10 > 0 and amountGiven+10 <= int(num):
+                    givenNotes.append(10)
+                    currency10 -= 1
+                    amountGiven += 10
+                elif currency5 > 0 and amountGiven+5 <= int(num):
+                    givenNotes.append(5)
+                    currency5 -= 1
+                    amountGiven += 5
+                elif currency1 > 0 and amountGiven+1 <= int(num):
+                    givenNotes.append(1)
+                    currency1 -= 1
+                    amountGiven += 1
+            print(givenNotes)
+            print(sum(givenNotes))
+        else:
+            print('wrong password')
     else:
-        print('wrong password')
-else:
-    print('Wrong username !!')
+        print('Wrong username !!')
