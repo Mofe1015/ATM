1ab5815125503faa65948f10ac612ceb65da0ede
mofeomotosho8@gmail.com <mofeomotosho8@gmail.com>
2022-06-18 23:54:43 -0300

    Veryfing balance and getting new balance

--- main.py
+++ main.py
@@ -23,30 +23,38 @@ while int(num) != 0:
         password = input('Please enter your password: ')
 
         if password == contas[1][userPosition]:
+            balance = contas[2][userPosition]
+
             print('Welcome', username)
-            print('Yout balance is $B', contas[2][userPosition])
+            print('Your balance is $B', balance)
 
-            num = (input('Please enter an ammount: '))
+            num = int(input('Please enter an ammount: '))
 
-            while amountGiven < int(num):
-                if currency50 > 0 and amountGiven+50 <= int(num):
-                    givenNotes.append(50)
-                    currency50 -= 1
-                    amountGiven += 50
-                elif currency10 > 0 and amountGiven+10 <= int(num):
-                    givenNotes.append(10)
-                    currency10 -= 1
-                    amountGiven += 10
-                elif currency5 > 0 and amountGiven+5 <= int(num):
-                    givenNotes.append(5)
-                    currency5 -= 1
-                    amountGiven += 5
-                elif currency1 > 0 and amountGiven+1 <= int(num):
-                    givenNotes.append(1)
-                    currency1 -= 1
-                    amountGiven += 1
-            print(givenNotes)
-            print(sum(givenNotes))
+            if balance > num:
+                while amountGiven < int(num):
+                    if currency50 > 0 and amountGiven+50 <= int(num):
+                        givenNotes.append(50)
+                        currency50 -= 1
+                        amountGiven += 50
+                    elif currency10 > 0 and amountGiven+10 <= int(num):
+                        givenNotes.append(10)
+                        currency10 -= 1
+                        amountGiven += 10
+                    elif currency5 > 0 and amountGiven+5 <= int(num):
+                        givenNotes.append(5)
+                        currency5 -= 1
+                        amountGiven += 5
+                    elif currency1 > 0 and amountGiven+1 <= int(num):
+                        givenNotes.append(1)
+                        currency1 -= 1
+                        amountGiven += 1
+
+                newBalance = balance - num
+                print('\n\nYour new balance is $B', newBalance)
+                print(givenNotes)
+                print(sum(givenNotes))
+            else:
+                print("Sorry your balance is too low")
         else:
             print('wrong password')
     else:
